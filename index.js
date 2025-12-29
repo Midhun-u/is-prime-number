141316f63b68e462172f50f3b56c13d0bf0909d9
Midhun-u <midhunvkm689@gmail.com>
2025-12-29 10:28:23 +0530

    Updated code

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const isPrimeNumber = (number) => {
         throw new Error("The value should be a number")
     }
 
-    if (number <= 1) {
+    if (number <= 1) { 
         return false
     }
 
